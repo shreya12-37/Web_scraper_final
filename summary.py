62d6788c09b10659281a279eceea58fc821a503f
Rishab Goyal <rishabgoyal432@gmail.com>
2021-06-04 17:32:22 +0530

    Update summary.py

--- summary.py
+++ summary.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 def reviewScraper(url, page_no):
 
-    my_url = url 
+    my_url = url
     #print(htmlcontent)
     base_url = my_url + "&pageNumber="
     # page = 1
     response = []
-    print(page_no, "\n", "extraaaaaa")
-    for page in range(1,int(page_no)+1):
+    print(page_no,"extraaaaaa")
+    for page in range(1, int(page_no) + 1):
+        print("Running ",page)
     # while page != page_no:
         star_rating = []
         reviewerName = []
@@ -17,29 +18,31 @@ def reviewScraper(url, page_no):
         title = []
         url = base_url + str(page)
         r = requests.get(url)
-        # print(url)
+        print(url)
         htmlcontent = r.content
         soup = BeautifulSoup(htmlcontent, 'html.parser')
-        for tag in soup.find_all(class_="review-rating"):
-            star_rating.append(tag.get_text(strip=True))
-        # print(len(star_rating))
-        for tag in soup.find_all(class_="review-text-content"):
-            comments.append(tag.get_text(strip=True))
-        # print(len(comments))
-        for tag in soup.find_all(class_="a-profile-name"):
-            reviewerName.append(tag.get_text(strip=True))
-        # print(len(reviewerName))
-        for tag in soup.find_all(class_="review-date"):
-            date.append(tag.get_text(strip=True))
-        # print(len(date))
-        for tag in soup.find_all(class_="review-title"):
-            title.append(tag.get_text(strip=True))
-        # print(len(title))
-        m = min(len(comments),len(date),len(title),len(reviewerName),len(star_rating))
-        for i in range(0,m):
+        reviewDiv = soup.find_all("div", {"id": "cm_cr-review_list"})
+        for Div in reviewDiv:
+            for tag in Div.find_all(class_="review-rating"):
+                star_rating.append(tag.get_text(strip=True))
+            print(len(star_rating))
+            for tag in Div.find_all(class_="review-text-content"):
+                comments.append(tag.get_text(strip=True))
+            print(len(comments))
+            for tag in Div.find_all(class_="a-profile-name"):
+                reviewerName.append(tag.get_text(strip=True))
+            print(len(reviewerName))
+            for tag in Div.find_all(class_="review-date"):
+                date.append(tag.get_text(strip=True))
+            print(len(date))
+            for tag in Div.find_all(class_="review-title"):
+                title.append(tag.get_text(strip=True))
+            print(len(title))
+        print("\n")
+        for i in range(0,len(comments)):
             my_dict={"name": reviewerName[i],"stars": star_rating[i],"date": date[i],"title": title[i], "review": comments[i]}
             response.append(my_dict)
 
         # page = page+1
     print("LENGTH ",len(response))
-    return response 
+    return response
